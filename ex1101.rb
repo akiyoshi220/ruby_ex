6d6b5eb886f5e88ad0175d87756114382a727710
akiyoshi koike <a_koike@macmini-09f895.active.co.jp>
2013-03-26 15:05:05 +0900

    追加でコミット

---
+++ ex1101.rb
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+friends = {
+	:shin => "Shin Kuboaki",
+	:shinichirou => "Shinichirou Ooba",
+	:shingo => "Shingo Katori"
+	}
+
+friends.each { |key, value|
+	puts "#{key}: #{value}"
+}
---
+++ ex1104.rb
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+str = "Twinkle, twinkle, little star,
+How I wonder What you are.
+Up above the world so high,
+Like a diamond in the sky.
+Twinkle, twinkle, little star,
+How I wonder what you are.."
+
+lines = str.split(/\n/)
+
+puts "youが含まれていた行"
+lines.each { |line|
+	if line =~ /[Yy]ou/ then
+		puts line
+	end
+}
+
+puts ""
+
+puts "行馬tにカンマが含まれていた行"
+lines.each { |line|
+	if line =~ /,$/ then
+		puts line
+	end
+}
+
+puts ""
+
+puts '"i"がきて2文字おいて"l"がくる文字列が含まれる行'
+lines.each { |line|
+	if line =~ /i..l/ then
+		puts line
+	end
+}
